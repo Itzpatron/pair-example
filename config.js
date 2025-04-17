efe3e7174e597fa9621bb32bf423e09a45d86d69
PATRON <137694181+Itzpatron@users.noreply.github.com>
2025-04-17 22:42:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "PATRON",
+  PREFIX: process.env.PREFIX || "PATRON-MD~",
   EMAIL: process.env.EMAIL || "maximusfadare@gmail.com",
   PASS: process.env.PASS || "Maximus0000.",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
